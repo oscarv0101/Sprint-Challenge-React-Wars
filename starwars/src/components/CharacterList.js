22733c9fa5d82032abcd8e7e0d14ffc2f874678a
milkslapper <oscar.valenzuela69@gmail.com>
2019-05-17 19:47:21 -0700

    rendered all data to screen

---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Character from "../components/Character"
+import "./StarWars.css"
+
+function CharacterList(props){
+    return(
+        <div>
+            {props.char.map(character => {
+                return (
+                    <Character char ={character}/>
+                )
+            })}
+        </div>
+    )
+}
+
+
+
+
+export default CharacterList;
